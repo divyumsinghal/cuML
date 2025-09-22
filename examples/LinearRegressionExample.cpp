1b63c33d1cc1ac6f510ea9205c030023f5fa0a7d
Divyum Singhal <divyumsinghal@gmail.com>
2025-09-22 22:01:10 +0100

    Fixes

--- examples/LinearRegressionExample.cpp
+++ examples/LinearRegressionExample.cpp
@@ -3,7 +3,7 @@
 #include <vector>
 
 int main() {
-  int m = 4, n = 4;
+  int m = 5, n = 3;
 
   /*
    *   A = |  1.0 |  5.0 |  9.0 | 13.0 |
@@ -14,10 +14,10 @@ int main() {
    *   B = |  1.0 |  2.0 |  3.0 |  4.0 |
    */
 
-  const std::vector<float> A = {1.0f,  2.0f,  3.0f,  4.0f,  5.0f,  6.0f,
-                                7.0f,  8.0f,  9.0f,  10.0f, 11.0f, 12.0f,
-                                13.0f, 14.0f, 15.0f, 16.0f};
-  const std::vector<float> B = {1.0f, 2.0f, 3.0f, 4.0f};
+  const std::vector<float> A = {1.0f,  2.0f,  3.0f,  4.0f,  5.0f,
+                                6.0f,  7.0f,  8.0f,  9.0f,  10.0f,
+                                11.0f, 12.0f, 13.0f, 14.0f, 15.0f};
+  const std::vector<float> B = {18.0f, 21.0f, 24.0f, 27.0f, 30.0f};
 
   /*
    *   C = | 276.0 | 304.0 | 332.0 |
--- utils/cusolver_utils.h
+++ utils/cusolver_utils.h
@@ -1,5 +1,11 @@
 #pragma once
 
+#include <cuComplex.h>
+#include <cublas_api.h>
+#include <cuda_runtime_api.h>
+#include <cusolverDn.h>
+#include <library_types.h>
+
 #include <cmath>
 #include <functional>
 #include <iostream>
@@ -7,38 +13,12 @@
 #include <stdexcept>
 #include <string>
 
-#include <cuComplex.h>
-#include <cuda_runtime_api.h>
-#include <cublas_api.h>
-#include <cusolverDn.h>
-#include <library_types.h>
-
-
 // cusolver API error checking
-#define CUSOLVER_CHECK(err)                                                                        \
-    do {                                                                                           \
-        cusolverStatus_t err_ = (err);                                                             \
-        if (err_ != CUSOLVER_STATUS_SUCCESS) {                                                     \
-            printf("cusolver error %d at %s:%d\n", err_, __FILE__, __LINE__);                      \
-            throw std::runtime_error("cusolver error");                                            \
-        }                                                                                          \
-    } while (0)
-
-// Returns cusolverIRSRefinement_t value as defined in cusolver_common.h for the string containing
-// solver name
-cusolverIRSRefinement_t get_cusolver_refinement_solver(std::string solver_string) {
-    if (solver_string.compare("CUSOLVER_IRS_REFINE_NONE") == 0)
-        return CUSOLVER_IRS_REFINE_NONE;
-    else if (solver_string.compare("CUSOLVER_IRS_REFINE_CLASSICAL") == 0)
-        return CUSOLVER_IRS_REFINE_CLASSICAL;
-    else if (solver_string.compare("CUSOLVER_IRS_REFINE_GMRES") == 0)
-        return CUSOLVER_IRS_REFINE_GMRES;
-    else if (solver_string.compare("CUSOLVER_IRS_REFINE_CLASSICAL_GMRES") == 0)
-        return CUSOLVER_IRS_REFINE_CLASSICAL_GMRES;
-    else if (solver_string.compare("CUSOLVER_IRS_REFINE_GMRES_GMRES") == 0)
-        return CUSOLVER_IRS_REFINE_GMRES_GMRES;
-    else
-        printf("Unknown solver parameter: \"%s\"\n", solver_string.c_str());
-
-    return CUSOLVER_IRS_REFINE_NOT_SET;
-}
+#define CUSOLVER_CHECK(err)                                             \
+  do {                                                                  \
+    cusolverStatus_t err_ = (err);                                      \
+    if (err_ != CUSOLVER_STATUS_SUCCESS) {                              \
+      printf("cusolver error %d at %s:%d\n", err_, __FILE__, __LINE__); \
+      throw std::runtime_error("cusolver error");                       \
+    }                                                                   \
+  } while (0)
