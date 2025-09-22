550a4789bda11af92c405228a74ceb1b0b6f2c0a
Divyum Singhal <divyumsinghal@gmail.com>
2025-09-22 22:06:59 +0100

    Fix

--- examples/LinearRegressionExample.cpp
+++ examples/LinearRegressionExample.cpp
@@ -6,30 +6,34 @@ int main() {
   int m = 5, n = 3;
 
   /*
-   *   A = |  1.0 |  5.0 |  9.0 | 13.0 |
+   *   X = |  1.0 |  5.0 |  9.0 | 13.0 |
    *       |  2.0 |  6.0 | 10.0 | 14.0 |
    *       |  3.0 |  7.0 | 11.0 | 15.0 |
    *       |  4.0 |  8.0 | 12.0 | 16.0 |
    *
-   *   B = |  1.0 |  2.0 |  3.0 |  4.0 |
+   *   Y = |  18.0 |  21.0 |  24.0 |  27.0 |
+   *
+   *   Y = [1, 1, 1, 1] X
+   *   W = [1, 1, 1, 1]
    */
 
-  const std::vector<float> A = {1.0f,  2.0f,  3.0f,  4.0f,  5.0f,
+  const std::vector<float> X = {1.0f,  2.0f,  3.0f,  4.0f,  5.0f,
                                 6.0f,  7.0f,  8.0f,  9.0f,  10.0f,
                                 11.0f, 12.0f, 13.0f, 14.0f, 15.0f};
-  const std::vector<float> B = {18.0f, 21.0f, 24.0f, 27.0f, 30.0f};
+  const std::vector<float> Y = {18.0f, 21.0f, 24.0f, 27.0f, 30.0f};
 
   /*
-   *   C = | 276.0 | 304.0 | 332.0 |
+   *   W = | 1.0 | 1.0 | 1.0 | 1.0 |
    */
-  std::vector<float> C(m * n, 0.0f);
+  std::vector<float> W(m * n, 0.0f);
 
   LinearRegression<float> lr;
-  lr.fit(A, B, C, 4, 3);
+  lr.fit(X, Y, W, 4, 3);
 
-  for (int i : C) {
+  for (int i : W) {
     std::cout << i << " ";
   }
+
   std::cout << std::endl;
 
   return 0;
